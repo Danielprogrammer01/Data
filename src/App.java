5f36c940b9eab2337e761f91af72e53c0f0d24af
Danielprogrammer01 <dannielsantoscorreia@gmail.com>
2024-09-06 01:32:44 -0300

    Conclusão do exercício

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    
+        Data dataOriginal = new Data(LocalDate.of(2024, 9, 6));
+        Data dataClonada = dataOriginal.clone();
+
+        System.out.println("Data Original: " + dataOriginal.getData());
+        System.out.println("Data Clonada: " + dataClonada.getData());
+        System.out.println(dataOriginal.getMesExtenso());
+        System.out.println(dataOriginal.compararData(dataClonada));
+        System.out.println(dataOriginal.isBissexto());
+
     }
 }
