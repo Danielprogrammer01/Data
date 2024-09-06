ea03852b5f84abba75fe3a6269a53e5814933d53
Danielprogrammer01 <dannielsantoscorreia@gmail.com>
2024-09-06 13:32:58 -0300

    alteraçao para switch case

--- src/App.java
+++ src/App.java
@@ -5,10 +5,11 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     
-        Data dataOriginal = new Data(LocalDate.of(2024, 9, 6));
+        Data dataOriginal = new Data(LocalDate.of(2024, 9, 7));
         Data dataClonada = dataOriginal.clone();
 
-        System.out.println("Data Original: " + dataOriginal.getData());
+        System.out.println("Data Atual: " + dataOriginal.getDataAtual());
+        System.out.println("Data Recebida: " + dataOriginal.getData());
         System.out.println("Data Clonada: " + dataClonada.getData());
         System.out.println(dataOriginal.getMesExtenso());
         System.out.println(dataOriginal.compararData(dataClonada));
